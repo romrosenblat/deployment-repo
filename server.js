bac1f2c7b341b4047423315b68d7fccf724833a9
romrosenblat <romrosenblat@gmail.com>
2018-01-01 16:19:24 +0200

    handleHowAreYou

--- server.js
+++ server.js
@@ -1,21 +1,31 @@
 'use strict'
 const Slapp = require('slapp')
+const express = require('express')
+const BeepBoopConvoStore = require('slapp-convo-beepboop')
 const BeepBoopContext = require('slapp-context-beepboop')
 if (!process.env.PORT) throw Error('PORT missing but required')
 
-var slapp = Slapp({ context: BeepBoopContext() })
+var slapp = Slapp({ 
+	convo_store: BeepBoopConvoStore(),
+	context: BeepBoopContext() 
+
+})
+
+var app = slapp.attachToExpress(express())
 
 slapp.message('^(hi|hello|hey).*', ['direct_mention', 'direct_message'], (msg, text, greeting) => {
-  msg
-    .say(`${greeting}, how are you?`)
-    .route('handleHowAreYou')  // where to route the next msg in the conversation
+  msg.say('how are you?').route('handleHowAreYou',{what: match1})  // where to route the next msg in the conversation
 })
 
 // register a route handler
-slapp.route('handleHowAreYou', (msg) => {
+slapp.route('handleHowAreYou', (msg,state) => {
   // respond with a random entry from array
-  msg.say(['Me too', 'Noted', 'That is interesting'])
+  msg.say(['Hi' + state.what])
 })
 
 // attach handlers to an Express app
 slapp.attachToExpress(require('express')()).listen(process.env.PORT)
+
+
+console.log('Listening on :' + process.env.PORT)
+app.listen(process.env.PORT)
